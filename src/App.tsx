e3f2dc63e0bbffaf039e6008169ada9f3252a947
Okelekele Nobel Bobby <bsoftlimited@gmail.com>
2023-01-15 17:48:00 +0100

    added some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -8,12 +12,42 @@ import TextField from '@mui/material/TextField';
 import './App.css';
 
 const App = () => {
-  return <form>
-    <label>My Rent</label>
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-  </form>;
+  return return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br />
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>);
 }
 
 export default App;
---
+++ src/components/forms/name.ts
@@ -0,0 +1,8 @@
+export default function Name() {
+    return <form>
+        <label>My Rent</label>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>;
+}
