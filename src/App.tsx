1f206d36d09fca554da141bfe05354d085f1cffa
BsoftLimited <bsoftlimited@gmail.com>
2023-01-15 17:35:20 +0100

    just installed bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import TextField from '@mui/material/TextField';
 import './App.css';
--- src/items/user.ts
+++ src/items/user.ts
@@ -1,7 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+export interface Plan{
+    id: string, salary: number, rent: number, months: number
+}
+
 export interface User{
-    id: string, name: string, username: string
+    id: string, name: string, username: string, plan?: Plan 
 }
 
 export interface UserInfo{
@@ -13,13 +17,26 @@ const initialState: UserInfo = { active: false, user: undefined};
 export const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers:{
+    reducers: {
+        signup: (state: UserInfo, action: PayloadAction<{ name: string, username: string, password: string}>) =>{
+            let id = "cdfghjkl";
+            return { active: true, user: { id, name: action.payload.name, username: action.payload.username}};
+        },
         login: (state: UserInfo, action: PayloadAction<{ username: string, password: string}>) =>{
             let id = "cdfghjkl";
             return { active: true, user: { id, name: "Okelekele", username: action.payload.username}};
         },
         logout: (state: UserInfo, action: PayloadAction) =>{
             return { active: false, user: undefined};
+        },
+        set: (state: UserInfo, action: PayloadAction<{ salary: number, rent: number, months: number }>) =>{
+            let id = "cdfghjkl";
+            if (state.user) {
+                let init = { ...state.user };
+                init.plan = { id, salary: action.payload.salary, rent: action.payload.rent, months: action.payload.months };
+                return { ...state, user: init };
+            }
+            return state;
         },
     }
 });
